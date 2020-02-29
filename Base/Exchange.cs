26db253c5b6a572843a9f0b8aa2cf94f760165fd
Elisy <ebishkek@yandex.ru>
2020-02-29 13:47:16 +0600

    Exchange base class

---
+++ Base/Exchange.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AgentBit.Ccxt
+{
+
+    /// <summary>
+    /// Base class for all exchanges
+    /// </summary>
+    public class Exchange: IDisposable
+    {
+        public TimeSpan Timeout { get; set; }
+
+        public SocketsHttpHandler SocketsHttpHandler { get; set; }
+
+        protected HttpClient _httpClient = null;
+        public HttpClient HttpClient
+        {
+            get
+            {
+                if (_httpClient == null)
+                { 
+                    _httpClient = new HttpClient(SocketsHttpHandler);
+                    _httpClient.Timeout = Timeout;
+                }
+
+                return _httpClient;
+            }
+        }
+
+        public Exchange()
+        {
+            SocketsHttpHandler = new SocketsHttpHandler();
+            SocketsHttpHandler.MaxConnectionsPerServer = 5;
+            SocketsHttpHandler.UseCookies = false;
+
+            Timeout = TimeSpan.FromSeconds(5);
+        }
+
+
+        /// <summary>
+        /// Request rate limiter
+        /// </summary>
+        public virtual async Task Throttle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Sign()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            if (_httpClient != null)
+                _httpClient.Dispose();
+        }
+    }
+}
---
+++ Base/Interfaces.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgentBit.Ccxt.Base
+{
+    public interface ICancelAllOrders
+    {
+    }
+
+    public interface ICancelOrder
+    {
+    }
+
+    public interface ICancelOrders
+    {
+    }
+
+    public interface ICORS
+    {
+    }
+
+    public interface ICreateDepositAddress
+    {
+    }
+
+    public interface ICreateLimitOrder
+    {
+    }
+
+    public interface ICreateMarketOrder
+    {
+    }
+
+    public interface ICreateOrder
+    {
+    }
+
+    public interface IDeposit
+    {
+    }
+
+    public interface IEditOrder
+    {
+    }
+
+    public interface IFetchBalance
+    {
+    }
+
+    public interface IFetchBidsAsks
+    {
+    }
+
+    public interface IFetchClosedOrders
+    {
+    }
+
+    public interface IFetchCurrencies
+    {
+    }
+
+    public interface IFetchDepositAddress
+    {
+    }
+
+    public interface IFetchDeposits
+    {
+    }
+
+    public interface IFetchFundingFees
+    {
+    }
+
+    public interface IFetchL2OrderBook
+    {
+    }
+
+    public interface IFetchLedger
+    {
+    }
+
+    public interface IFetchMarkets
+    {
+    }
+
+    public interface IFetchMyTrades
+    {
+    }
+
+    public interface IFetchOHLCV
+    {
+    }
+
+    public interface IFetchOpenOrders
+    {
+    }
+
+    public interface IFetchOrder
+    {
+    }
+
+    public interface IFetchOrderBook
+    {
+    }
+
+    public interface IFetchOrderBooks
+    {
+    }
+
+    public interface IFetchOrders
+    {
+    }
+
+    public interface IFetchOrderTrades
+    {
+    }
+
+    public interface IFetchStatus
+    {
+    }
+
+    public interface IFetchTicker
+    {
+    }
+
+    public interface IFetchTickers
+    {
+    }
+
+    public interface IFetchTime
+    {
+    }
+
+    public interface IFetchTrades
+    {
+    }
+
+    public interface IFetchTradingFee
+    {
+    }
+
+    public interface IFetchTradingFees
+    {
+    }
+
+    public interface IFetchTradingLimits
+    {
+    }
+
+    public interface IFetchTransactions
+    {
+    }
+
+    public interface IFetchWithdrawals
+    {
+    }
+
+    public interface IPrivateAPI
+    {
+    }
+
+    public interface IPublicAPI
+    {
+    }
+
+    public interface IWithdraw
+    {
+    }
+}
