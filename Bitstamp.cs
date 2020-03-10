b34ffdd243db33ba3f4353f89a872e2c57db9469
elisy <ebishkek@yandex.ru>
2020-03-10 22:10:45 +0600

    Bitstamp fetchticker

---
+++ Bitstamp.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using AgentBit.Ccxt.Base;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AgentBit.Ccxt
+{
+    public class Bitstamp : Exchange, IPublicAPI, IPrivateAPI, IFetchTickers, IFetchTicker
+    {
+        readonly Uri ApiPublicV2 = new Uri("https://www.bitstamp.net/api/v2/");
+
+        public Bitstamp() : base()
+        {
+            //https://www.bitstamp.net/api/
+            //Do not make more than 8000 requests per 10 minutes or we will ban your IP address.
+            RateLimit = 10 * 60 / 8000 * 1000;
+        }
+
+        public override async Task<Market[]> FetchMarkets()
+        {
+            return await Exchange.MemoryCache.GetOrCreateAsync<Market[]>($"{GetType().FullName}.FetchMarkets", async entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24);
+
+                var pairsResponse = await Request(new Base.Request()
+                {
+                    BaseUri = ApiPublicV2,
+                    Path = "trading-pairs-info",
+                    ApiType = "public",
+                    Method = HttpMethod.Get,
+                    Timeout = TimeSpan.FromSeconds(30)
+                });
+                var pairs = JsonSerializer.Deserialize<PairsInfo[]>(pairsResponse.Text);
+
+                var result = new List<Market>();
+
+                foreach(var market in pairs)
+                {
+                    var newItem = new Market();
+
+                    var name = market.name.Split('/');
+                    newItem.BaseId = name[0];
+                    newItem.QuoteId = name[1];
+                    newItem.Id = newItem.BaseId + "/" + newItem.QuoteId;
+                    newItem.Base = GetCommonCurrencyCode(newItem.BaseId);
+                    newItem.Quote = GetCommonCurrencyCode(newItem.QuoteId);
+                    newItem.Symbol = newItem.Base + "/" + newItem.Quote;
+                    newItem.AmountPrecision = market.base_decimals;
+                    newItem.PricePrecision = market.counter_decimals;
+                    newItem.Active = market.trading == "Enabled";
+
+                    var parts = market.minimum_order.Split(' ');
+                    newItem.CostMin = JsonSerializer.Deserialize<double>(parts[0]);
+
+                    result.Add(newItem);
+                }
+
+                return result.ToArray();
+            });
+        }
+
+        public override void Sign(Request request)
+        {
+            if (request.ApiType != "private")
+                return;
+        }
+
+        public async Task<Ticker> FetchTicker(string symbol)
+        {
+            var market = (await FetchMarkets()).FirstOrDefault(m => m.Symbol == symbol);
+            if (market == null)
+                return null;
+
+            var response = await Request(new Base.Request()
+            {
+                BaseUri = ApiPublicV2,
+                Path = $"ticker/{market.BaseId.ToLower()}{market.QuoteId.ToLower()}/",
+                Method = HttpMethod.Get,
+                Timeout = TimeSpan.FromSeconds(5)
+            });
+
+            var ticker = JsonSerializer.Deserialize<BitstampTicker>(response.Text);
+            
+            var result = new Ticker();
+            result.Symbol = market.Symbol;
+            result.Timestamp = Convert.ToUInt64(JsonSerializer.Deserialize<double>(ticker.timestamp));
+            result.DateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(result.Timestamp);
+            result.High = JsonSerializer.Deserialize<double>(ticker.high);
+            result.Low = JsonSerializer.Deserialize<double>(ticker.low);
+            result.Bid = JsonSerializer.Deserialize<double>(ticker.bid);
+            result.Ask = JsonSerializer.Deserialize<double>(ticker.ask);
+            result.Vwap = JsonSerializer.Deserialize<double>(ticker.vwap);
+            result.Open = JsonSerializer.Deserialize<double>(ticker.open);
+            result.Last = JsonSerializer.Deserialize<double>(ticker.last);
+            result.Close = result.Last;
+            result.BaseVolume = JsonSerializer.Deserialize<double>(ticker.volume);
+            result.QuoteVolume = result.BaseVolume * result.Vwap;
+            result.Info = ticker;
+
+            return result;
+        }
+
+        public async Task<Ticker[]> FetchTickers(string[] symbols = null)
+        {
+            if (symbols == null)
+                symbols = (await FetchMarkets()).Select(m => m.Symbol).ToArray();
+            var result = new List<Ticker>(symbols.Length);
+            foreach (var symbol in symbols)
+                result.Add(await FetchTicker(symbol));
+            return result.ToArray();
+        }
+
+        public class PairsInfo
+        {
+            public string name { get; set; }
+            public int base_decimals { get; set; }
+            public string minimum_order { get; set; }
+            public int counter_decimals { get; set; }
+            public string trading { get; set; }
+            public string url_symbol { get; set; }
+            public string description { get; set; }
+        }
+
+        public class BitstampTicker
+        {
+            //{"high": "8158.42", "last": "8033.58", "timestamp": "1583853488", "bid": "8033.70", "vwap": "7901.42", "volume": "8157.55847320", "low": "7636.00", "ask": "8044.18", "open": "7937.20"}
+            public string high { get; set; }
+            public string last { get; set; }
+            public string timestamp { get; set; }
+            public string bid { get; set; }
+            public string vwap { get; set; }
+            public string volume { get; set; }
+            public string low { get; set; }
+            public string ask { get; set; }
+            public string open { get; set; }
+        }
+
+    }
+}
