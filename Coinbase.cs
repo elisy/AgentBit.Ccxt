5ec97dcdfe2a1833552ffbd7f18c20268ee194d8
Sergei Kartashev <sergeikartashev@tengizchevroil.com>
2021-12-27 14:15:08 +0600

    FetchTickers implementation

--- Coinbase.cs
+++ Coinbase.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.WebSockets;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using AgentBit.Ccxt.Base;
@@ -22,6 +25,7 @@ namespace AgentBit.Ccxt
     public class Coinbase : Exchange, IPublicAPI, IFetchTickers, IFetchTicker
     {
         readonly Uri ApiV2 = new Uri("https://api.exchange.coinbase.com/");
+        readonly Uri WebSocketUri = new Uri("wss://ws-feed.exchange.coinbase.com");
 
         public Coinbase(HttpClient httpClient, ILogger logger) : base(httpClient, logger)
         {
@@ -91,13 +95,140 @@ namespace AgentBit.Ccxt
 
         public async Task<Ticker> FetchTicker(string symbol)
         {
+            //var market = (await FetchMarkets().ConfigureAwait(false)).FirstOrDefault(m => m.Symbol == symbol);
+            //if (market == null)
+            //    return null;
+
+            //var response = await Request(new Base.Request()
+            //{
+            //    BaseUri = ApiV2,
+            //    Path = $"products/{market.Id}/stats",
+            //    ApiType = "public",
+            //    Method = HttpMethod.Get
+            //}).ConfigureAwait(false);
+
+            //var responseJson = JsonSerializer.Deserialize<CoinbaseProduct[]>(response.Text);
             return (await FetchTickers(new string[] { symbol }).ConfigureAwait(false)).FirstOrDefault();
         }
 
+
         public async Task<Ticker[]> FetchTickers(string[] symbols = null)
         {
-            throw new NotImplementedException();
+            var markets = (await FetchMarkets().ConfigureAwait(false)).Where(m => m.Active).ToArray();
+            var result = markets.ToDictionary(m => m.Symbol, m => (Ticker)null);
+
+            var jsonSubscribe = new
+            {
+                type = "subscribe",
+                product_ids = markets.Select(m => m.Id).ToArray(),
+                channels = new object[]
+                {
+                    new
+                    {
+                        name = "ticker",
+                        product_ids = markets.Select(m => m.Id).ToArray()
+                    }
+                }
+            };
+
+            ArraySegment<byte> bytesToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(jsonSubscribe)));
+            using (ClientWebSocket socket = new ClientWebSocket())
+            {
+                await socket.ConnectAsync(WebSocketUri, CancellationToken.None);
+                await socket.SendAsync(bytesToSend, WebSocketMessageType.Text, true, CancellationToken.None);
+
+                ArraySegment<byte> bufferToReceive = new ArraySegment<byte>(new byte[8192]);
+
+                var totalIterations = 0;
+                while (socket.State == WebSocketState.Open && totalIterations++ < markets.Length * 2)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        WebSocketReceiveResult socketResult = null;
+                        do
+                        {
+                            socketResult = await socket.ReceiveAsync(bufferToReceive, CancellationToken.None);
+                            ms.Write(bufferToReceive.Array, bufferToReceive.Offset, socketResult.Count);
+                        }
+                        while (!socketResult.EndOfMessage);
+
+                        ms.Seek(0, SeekOrigin.Begin);
+
+                        var stringJson = Encoding.UTF8.GetString(ms.ToArray());
+                        var resultJson = JsonSerializer.Deserialize<Dictionary<string, object>>(stringJson);
+
+                        if (resultJson["type"].ToString() == "error")
+                        {
+                            _logger.LogError($"message: {resultJson["message"]} {stringJson}");
+                        }
+                        else if (resultJson["type"].ToString() == "ticker")
+                        {
+                            try
+                            {
+                                var jsonTicker = JsonSerializer.Deserialize<CoinbaseSocketTicker>(stringJson);
+                                var market = markets.FirstOrDefault(m => m.Id == jsonTicker.product_id);
+                                if (market == null)
+                                    continue;
+
+                                //Some of ticker string fields may be null
+                                if (String.IsNullOrEmpty(jsonTicker.time))
+                                    continue;
+
+                                //var ticker = new Ticker()
+                                //{
+                                //    Symbol = market.Symbol,
+                                //    DateTime = DateTime.Parse(jsonTicker.time, null, System.Globalization.DateTimeStyles.RoundtripKind),
+                                //    High = Convert.ToDecimal(jsonTicker.high_24h, CultureInfo.InvariantCulture),
+                                //    Low = Convert.ToDecimal(jsonTicker.low_24h, CultureInfo.InvariantCulture),
+                                //    Bid = Convert.ToDecimal(jsonTicker.best_bid, CultureInfo.InvariantCulture),
+                                //    Ask = Convert.ToDecimal(jsonTicker.best_ask, CultureInfo.InvariantCulture),
+                                //    Open = Convert.ToDecimal(jsonTicker.open_24h, CultureInfo.InvariantCulture), //"open_24h":"7.3e-7"
+                                //    Close = Convert.ToDecimal(jsonTicker.price, CultureInfo.InvariantCulture),
+                                //    Last = Convert.ToDecimal(jsonTicker.price, CultureInfo.InvariantCulture),
+                                //    BaseVolume = Convert.ToDecimal(jsonTicker.volume_24h, CultureInfo.InvariantCulture),
+                                //    Info = jsonTicker
+                                //};
+                                var ticker = new Ticker()
+                                {
+                                    Symbol = market.Symbol,
+                                    DateTime = DateTime.Parse(jsonTicker.time, null, System.Globalization.DateTimeStyles.RoundtripKind),
+                                    High = decimal.Parse(jsonTicker.high_24h, NumberStyles.Float, CultureInfo.InvariantCulture),
+                                    Low = decimal.Parse(jsonTicker.low_24h, NumberStyles.Float, CultureInfo.InvariantCulture),
+                                    Bid = decimal.Parse(jsonTicker.best_bid, NumberStyles.Float, CultureInfo.InvariantCulture),
+                                    Ask = decimal.Parse(jsonTicker.best_ask, NumberStyles.Float, CultureInfo.InvariantCulture),
+                                    Open = decimal.Parse(jsonTicker.open_24h, NumberStyles.Float, CultureInfo.InvariantCulture), //"open_24h":"7.3e-7"
+                                    Close = decimal.Parse(jsonTicker.price, NumberStyles.Float, CultureInfo.InvariantCulture),
+                                    Last = decimal.Parse(jsonTicker.price, NumberStyles.Float, CultureInfo.InvariantCulture),
+                                    BaseVolume = decimal.Parse(jsonTicker.volume_24h, NumberStyles.Float, CultureInfo.InvariantCulture),
+                                    Info = jsonTicker
+                                };
+                                ticker.Timestamp = (uint)(ticker.DateTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                                result[market.Symbol] = ticker;
+                            }
+                            catch (Exception e)
+                            {
+                                _logger.LogError(e, $"Error parsint ticker {stringJson}");
+                            }
+                        }
+                        else if (resultJson["type"].ToString() == "subscriptions")
+                        {
+                            //"subscriptions" Message ends subscribing process. By this time server sent all tickers
+                            break;
+                        }
+                        else
+                        {
+                            _logger.LogError($"Unknown type: {resultJson["type"]} {stringJson}");
+                        }
+                    }
+                }
+            }
+
+            if (symbols == null)
+                return result.Values.Where(m => m != null).ToArray();
+            else
+                return result.Values.Where(m => m != null).Where(m => symbols.Contains(m.Symbol)).ToArray();
         }
+
 
         public override void Sign(Request request)
         {
@@ -154,6 +285,26 @@ namespace AgentBit.Ccxt
             public string status { get; set; }
             public string status_message { get; set; }
             public bool auction_mode { get; set; }
+        }
+
+        public class CoinbaseSocketTicker
+        {
+            public string type { get; set; }
+            public long sequence { get; set; }
+            public string product_id { get; set; }
+            public string price { get; set; }
+            public string open_24h { get; set; }
+            public string volume_24h { get; set; }
+            public string low_24h { get; set; }
+            public string high_24h { get; set; }
+            public string volume_30d { get; set; }
+            public string best_bid { get; set; }
+            public string best_ask { get; set; }
+            public string side { get; set; }
+            public string time { get; set; }
+            public int trade_id { get; set; }
+            public string last_size { get; set; }
+
         }
 
     }
